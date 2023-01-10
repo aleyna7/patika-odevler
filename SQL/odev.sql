db2ec0e59182365ae9559c119b350ddea40e146e
Aleyna Ozkan <111062676+aleynaozkan@users.noreply.github.com>
2023-01-10 21:53:06 +0300

    odev 10 eklendi

--- SQL/odev.sql
+++ SQL/odev.sql
@@ -287,3 +287,22 @@ INNER JOIN payment ON customer.payment_id = payment.payment_id;
 SELECT rental_id, first_name, last_name FROM customer
 INNER JOIN rental ON customer.rental_id = rental.rental_id;
 
+
+--ODEV 10--
+/*
+1- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+2- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+3- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+*/
+
+--1-
+SELECT city, country FROM city
+LEFT JOIN country ON city.country_id = city.country_id;
+
+--2-
+SELECT payment_id, first_name, last_name FROM payment
+RIGHT JOIN customer ON customer.payment_id = payment.payment_id;
+
+--3-
+SELECT rental_id, first_name, last_name FROM customer
+FULL JOIN rental ON customer.rental_id = rental.rental_id;
