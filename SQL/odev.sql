4e4cc0a77e3df8ded3c59bf19702eeb56053129d
Aleyna Ozkan <111062676+aleynaozkan@users.noreply.github.com>
2023-01-10 22:19:01 +0300

    odev 11 eklendi

--- SQL/odev.sql
+++ SQL/odev.sql
@@ -306,3 +306,37 @@ RIGHT JOIN customer ON customer.payment_id = payment.payment_id;
 --3-
 SELECT rental_id, first_name, last_name FROM customer
 FULL JOIN rental ON customer.rental_id = rental.rental_id;
+
+
+
+--ODEV 11--
+/*
+1- actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+2- actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+3- actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+4- İlk 3 sorguyu tekrar eden veriler için de yapalım.
+*/
+
+--1-
+(SELECT first_name FROM actor)
+UNION
+(SELECT first_name FROM customer);
+
+--2-
+(SELECT first_name FROM actor)
+INTERSECT
+(SELECT first_name FROM customer);
+
+--3-
+(SELECT first_name FROM actor)
+EXCEPT
+(SELECT first_name FROM customer);
+
+--4-
+(SELECT first_name FROM actor)
+UNION ALL
+(SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor)
+EXCEPT ALL
+(SELECT first_name FROM customer);
