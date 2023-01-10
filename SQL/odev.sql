5c6a7de61ba5779c503d86a35a5ec1a99ee11ce4
Aleyna Ozkan <111062676+aleynaozkan@users.noreply.github.com>
2023-01-11 00:03:11 +0300

    odev 12 eklendi

--- SQL/odev.sql
+++ SQL/odev.sql
@@ -340,3 +340,33 @@ UNION ALL
 (SELECT first_name FROM actor)
 EXCEPT ALL
 (SELECT first_name FROM customer);
+
+
+
+--ODEV 12--
+/*
+1- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+2- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+3- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+4- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+*/
+
+--1-
+SELECT COUNT(*) FROM film
+WHERE length > 
+(SELECT AVG(length) FROM film);
+
+--2-
+SELECT COUNT(*) FROM film
+WHERE rental_rate =
+(SELECT MAX(rental_rate) FROM film);
+
+--3-
+
+--4 but im not sure-
+SELECT customer.first_name, customer.last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment.amount =
+(
+    SELECT MAX(amount) FROM payment
+);
